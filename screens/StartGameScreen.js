87466ecf17cba572cdceb4d46f9731e19b9921e4
Sujata Regoti <regotiss@users.noreply.github.com>
2019-09-14 15:35:56 +0530

    Display input container as a shadow card

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TextInput, Button } from "react-native";
 
 const StartGameScreen = () => {
   return (
     <View style={styles.screen}>
-      <Text>Start Guess Game</Text>
+      <Text style={styles.title}>Start Guess Game</Text>
+      <View style={styles.inputContainer}>
+        <Text>Enter your number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Confirm" />
+          <Button title="Reset" />
+        </View>
+      </View>
     </View>
   );
 };
@@ -13,7 +21,29 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 20,
+    paddingVertical: 20
+  },
+  inputContainer: {
+    width: "80%",
     alignItems: "center",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: "white",
+    elevation: 5,
+    padding: 20,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 30
   }
 });
 export default StartGameScreen;
